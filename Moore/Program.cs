167b65ea3644947bea51987e1c97b57b49a3db1d
George Breje <george.breje2@gmail.com>
2020-10-09 14:46:04 +0300

    Rezolvare Moore

---
+++ Moore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int rezultatul = (int)(Math.Log(n,2)*18);
+            int ani = rezultatul / 12;
+            int luni = rezultatul % 12;
+            Console.WriteLine($"Puterea va fi de {n} ori mai mare decat in prezent peste {ani} ani si {luni} luni");
+
+        }
+    }
+}
