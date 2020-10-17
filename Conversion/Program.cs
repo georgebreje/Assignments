a83e553a23f15bad7a72cd2b41fbf1be2b55cebf
George Breje <george.breje2@gmail.com>
2020-10-17 17:04:20 +0300

    De la inceput

--- Conversion/Program.cs
+++ Conversion/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
 using System.Runtime.InteropServices;
+using System.IO; // pentru string
+using System.Security;
 
 namespace Conversion
 {
@@ -12,69 +14,22 @@ namespace Conversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Introduceti baza din care vreti sa convertiti ( intre 2 si 16 )");
-            int bazaInitiala = int.Parse(Console.ReadLine());   
+            //introducere
+            Console.WriteLine("------------------------");
+            Console.WriteLine("     Base converter");
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Introduceti baza din care vreti sa convertiti");
+            string bazaInit = Console.ReadLine();
             Console.WriteLine("Introduceti baza in care vreti sa convertiti");
             int bazaTinta = int.Parse(Console.ReadLine());
-            if(bazaTinta ==16 || bazaInitiala==16)
-
             Console.WriteLine("Introduceti numarul pe care vreti sa il convertiti");
-            double n = double.Parse(Console.ReadLine());
-            int parteIntreaga = (int)n;
-            double parteFractionara = n - (int)n;
-            int rest;
-            double produs = 0;
-            double parteFractionaraInitiala = n - (int)n;
-            Stack parteIntreagaConv = new Stack(); // stiva pentru afisare in ordine a valorii binare
-            Queue parteFractionaraConv = new Queue();
-            
-                    while (parteIntreaga != 0)
-                    {
-                        rest = parteIntreaga % bazaTinta;    
-                        parteIntreagaConv.Push(rest);
-                        parteIntreaga = (int)(parteIntreaga / bazaTinta);
-                    }
-                    while (parteIntreagaConv.Count != 0)
-                    {
-                        Console.Write(parteIntreagaConv.Peek());
-                        parteIntreagaConv.Pop();
-                    }
-                    if(parteFractionara!=0)
-                        Console.Write("."); // despartirea partii intregi de cea fractionara
-                    bool ok = true;
-                    while (parteFractionara != 0 && ok == true)
-                    {
-                        produs = parteFractionara * bazaTinta;
-                        //Console.Write($"{(int)produs}"); // afisare 0 / 1
-                        parteFractionaraConv.Enqueue((int)produs);
-                        parteFractionara = produs - (int)produs;
-                        if (parteFractionara - 0.0000000001 < parteFractionaraInitiala
-                            && parteFractionara + 0.0000000001 > parteFractionaraInitiala)
-                        {
-                            ok = false;
-                            break;
-                        }
-                    }
-                    if (ok == false && parteFractionaraInitiala != 0)
-                    {
-                        Console.Write("(");
-                        while (parteFractionaraConv.Count != 0)
-                        {
+            string numar = Console.ReadLine();
+            //sfarsit introducere
+            //trebuie sa aflam cate caractere sunt inainte si dupa punct
+            string[] split = numar.Split('.');
+            string parteIntreaga = split[0];
+            string parteFract = split[1];
 
-                            Console.Write(parteFractionaraConv.Peek());
-                            parteFractionaraConv.Dequeue();
-                        }
-                        Console.Write(")");
-                    }
-                    else
-                    {
-                        while (parteFractionaraConv.Count != 0 && parteFractionaraInitiala != 0)
-                        {
-                            Console.Write(parteFractionaraConv.Peek());
-                            parteFractionaraConv.Dequeue();
-                        }
-                    }
-                }
-            }
         }
-    
+    }
+}
