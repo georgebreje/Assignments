7550395bc0561ab5675053ba8b2c7a17c62e872e
George Breje <george.breje2@gmail.com>
2020-11-10 16:13:02 +0200

    add 2 columns

---
+++ HexViewer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HexViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti calea fisierului");
+            string path = Console.ReadLine();
+            char[] del = new char[] { ' ', '"' };
+            path = path.Trim(del);
+            int counter = 0;
+            byte[] ba = new byte[16];
+            int actual;
+            FileStream file = new FileStream(path, FileMode.Open);
+            while ((actual = file.Read(ba, 0, 16)) > 0)
+            {
+                string hex = BitConverter.ToString(ba);
+                string text = null;
+                for (int i = 0; i < actual; i++)
+                    (char)ba[i].ToString();
+
+                
+                
+                Console.WriteLine($"{counter:X8}: {hex} | {text}");
+                counter++;
+            }
+        }
+
+    }
+}
---
+++ pool3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace pool3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //problema1();
+            //problema2();
+            //problema3();
+
+        }
+        
+        private static void problema3()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] A = new int[n];
+            for(int i=0;i<n;i++)
+                A[i]= int.Parse(Console.ReadLine());
+        }
+
+        private static void problema2()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            int k = int.Parse(Console.ReadLine());
+            int ok = 0;
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < n && ok == 0; i++)
+            {
+                if (v[i] == k)
+                {
+                    ok = 1;
+                    Console.WriteLine(i);
+                }
+            }
+            if (ok == 0)
+                Console.WriteLine("-1");
+        }
+
+        private static void problema1()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            int s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+                s += v[i];
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
