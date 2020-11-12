d20444eb21b3b009d5084c8f2da939727b999b95
George Breje <george.breje2@gmail.com>
2020-11-12 16:32:08 +0200

    add 3

--- pool3/Program.cs
+++ pool3/Program.cs
@@ -14,17 +14,33 @@ namespace pool3
             //problema1();
             //problema2();
             //problema3();
+            problema4();
+        }
+
+        private static void problema4()
+        {
 
         }
-        
+
         private static void problema3()
         {
             int n = int.Parse(Console.ReadLine());
             int[] A = new int[n];
+            int max;
+            int min;
+            int count = 0;
             for(int i=0;i<n;i++)
                 A[i]= int.Parse(Console.ReadLine());
+            min = max=A[0];
+            for (int i = 1; i < n ; i++)
+            {
+                if (A[i] < min)
+                    min = A[i];
+                else if (A[i] > max)
+                    max = A[i];
+            }
+            Console.WriteLine($"min {min} max {max}");
         }
-
         private static void problema2()
         {
             int n = int.Parse(Console.ReadLine());
