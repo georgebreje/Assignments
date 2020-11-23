92beafe21f2500cdc06450ae3aa353ba8526dba5
George Breje <george.breje2@gmail.com>
2020-11-23 13:26:56 +0200

    del array until 13

--- pool2/Program.cs
+++ pool2/Program.cs
@@ -13,29 +13,29 @@ namespace pool2
     {
         static void Main(string[] args)
         {
-            problema1();
-            problema2();
-            problema3();
-            problema4();
-            problema5();
-            problema6();
-            problema7();
-            problema8();
-            problema9();
-            problema10();
-            problema11();
-            problema12();
-            problema13();
-            problema14();
-            problema15();
-            problema16();
-            problema17();
+            //problema1();
+            //problema2();
+            //problema3();
+            //problema4();
+            //problema5();
+            //problema6();
+            //problema7();
+            //problema8();
+            //problema9();
+            //problema10();
+            //problema11();
+            //problema12();
+            //problema13();
+            //problema14();
+            //problema15();
+            //problema16();
+            //problema17();
         }
 
         private static void problema17()
         {
             string sir = Console.ReadLine();
-            int k = 0, ok = 1 ,nr=0,maxv=-1;
+            int k = 0, ok = 1, nr = 0, maxv = -1;
             foreach (char c in sir)
             {
                 if (c == '0')
@@ -45,10 +45,10 @@ namespace pool2
                 else
                     ok = 0;
             }
-            if(ok!=0 && k==0)
+            if (ok != 0 && k == 0)
             {
                 Console.WriteLine("Este corect parantezat");
-                foreach(char c in sir)
+                foreach (char c in sir)
                 {
                     if (c == '0')
                         nr++;
@@ -111,7 +111,7 @@ namespace pool2
                         if (v[i] > maxim)
                         {
                             maxim2 = v[i];
-                            pozmax2= i;
+                            pozmax2 = i;
                         }
                     }
                     for (int i = 1; i < pozmax2; i++)
@@ -155,9 +155,9 @@ namespace pool2
             for (int i = 1; i < pozmax; i++)
                 if (v[i] >= v[i - 1])
                     ok++;
-            if(ok!=pozmax)
+            if (ok != pozmax)
                 Console.WriteLine("Nu este bitonica");
-            else if(ok==pozmax)
+            else if (ok == pozmax)
             {
                 for (int i = pozmax + 1; i < n - 1; i++)
                     if (v[i] < v[i + 1])
@@ -212,18 +212,21 @@ namespace pool2
                 else
                     Console.WriteLine("nu este o secventa monotona rotita");
             }
-            }
+        }
 
         private static void problema13()
         {
-            int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            int count = n-1;
+            /* int n = int.Parse(Console.ReadLine());
             int ok = 0;
             int ok1 = 0;
-            for (int i = 0; i < n; i++)
+            int aux;
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
             {
-                v[i] = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                aux = b;
+                b = a;
+                a = aux;
             }
             for (int i = 0; i < n-1; i++)
             {
@@ -252,234 +255,249 @@ namespace pool2
                     Console.WriteLine("este o secventa crescatoare rotita.");
                 else
                     Console.WriteLine("nu este o secventa crescatoare rotita.");
+        }
+        int n = int.Parse(Console.ReadLine());
+            int[] A = new int[n];
+            for (int i = 0; i < n; i++)
+                A[i] = int.Parse(Console.ReadLine());
+            int aux;
+            for (int i = 0; i < n - 1; i++)
+            {
+                aux = A[i];
+                A[i] = A[i + 1];
+                A[i + 1] = aux;
             }
+
+            for (int i = 0; i < n; i++)
+                Console.Write($"{A[i]} "); */
         }
 
-        private static void problema12()
+    private static void problema12()
+    {
+        string sir = Console.ReadLine();
+        string[] split = sir.Split('0');
+        int i = 0, grup = 0;
+        int prev = 0, cresc = 0, nr = 0;
+        for (i = 0; i < split.Length; i++)
         {
-            string sir = Console.ReadLine();
-            string[] split = sir.Split('0');
-            int i=0,ok=0,grup=0;
-
-            for(i=0;i<split.Length;i++)
+            cresc = 0;
+            nr = 0;
+            foreach (char c in split[i])
             {
-                ok = 0;
-                foreach(char c in split[i])
+
+                if (Char.IsDigit(c))
                 {
-                    if (Char.IsDigit(c))
-                        ok++;
-                }
-                //Console.WriteLine("SPLIT I "+split[i]); verificare pasi
-                //Console.WriteLine("OK "+ok);
-                if (ok != 0)
-                    grup++;
-                //Console.WriteLine("GRUP "+grup);
-            }
-            Console.WriteLine(grup);
-        }
-
-        private static void problema11()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            int s = 0;
-            int invers;
-            for (int i = 0; i < n; i++)
-            {
-                v[i] = int.Parse(Console.ReadLine());
-            }
-            for (int i = 0; i < n; i++)
-            {
-                invers = 0;
-                while(v[i]!=0)
-                {
-                    invers = invers*10 + v[i] % 10;
-                    v[i] = v[i] / 10;
-                }
-                s+=invers;
-            }
-            Console.WriteLine(s);
-        }
-
-        private static void problema10()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            int ok = 1, maxim = 0; ;
-            for (int i = 0; i < n; i++)
-            {
-                v[i] = int.Parse(Console.ReadLine());
-            }
-            for (int i = 0; i < n-1; i++)
-            {
-                if (v[i] == v[i + 1])
-                    ok++;
-                else
-                    ok = 1;
-                if (ok >= maxim)
-                    maxim = ok;
-            }
-            Console.WriteLine(maxim);
-        }
-
-        private static void problema9()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            int okc = 0;
-            for (int i = 0; i < n; i++)
-            {
-                v[i]= int.Parse(Console.ReadLine());
-            }
-            for (int i = 0; i < n-1; i++)
-            {
-                if (v[i] <= v[i + 1])
-                    okc = 0;
-                else
-                    okc++;
-            }
-            if(okc==0)
-                Console.WriteLine("Secventa monoton crescatoare");
-            else if(okc== n-1)
-                Console.WriteLine("Secventa monoton descrescatoare");
-            else
-                Console.WriteLine("Secventa nu este monotona");
-        }
-
-        private static void problema8()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            v[0] = 0;
-            v[1] = 1;
-            for (int i = 2; i < n; i++)
-            {
-                v[i] = v[i - 1] + v[i - 2];
-            }
-            Console.WriteLine($"al {n} lea nr este "+ v[n-1] );
-        }
-
-        private static void problema7()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            int minim, maxim;
-            for (int i = 0; i < n; i++)
-            {
-                v[i] = int.Parse(Console.ReadLine());
-            }
-            minim = v[0];
-            maxim = v[0];
-            for (int i = 1; i < n; i++)
-            {
-                if (v[i] < minim)
-                    minim = v[i];
-                if (v[i] > maxim)
-                    maxim = v[i];
-            }
-            Console.WriteLine("minim "+ minim);
-            Console.WriteLine("maxim "+ maxim);
-        }
-
-        private static void problema6()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            int nr = 0;
-            for (int i = 0; i < n; i++)
-            {
-                v[i] = int.Parse(Console.ReadLine());
-                if (v[i] >= v[i - 1])
-                    nr = 0;
-                else
-                    nr++;
-            }
-            if(nr==0)
-                Console.WriteLine("da");
-            else
-                Console.WriteLine("nu");
-        }
-
-        private static void problema5()
-        {
-            Console.WriteLine("n");
-            int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            int nr = 0;
-            for (int i = 0; i < n; i++)
-            {
-                v[i] = int.Parse(Console.ReadLine());
-                if (v[i] == i)
-                    nr++;
-            }
-            Console.WriteLine(nr);
-        }
-
-        private static void problema4()
-        {
-            Console.WriteLine("n");
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine("a");
-            int a = int.Parse(Console.ReadLine());
-            int ok = 0,poz =0;
-            int[] v = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                v[i] = int.Parse(Console.ReadLine());
-                if(v[i]==a)
-                {
-                    ok = 1;
-                    poz = i;
+                    if (c != '0')
+                    {
+                        if ((int)c > prev)
+                        {
+                            cresc++;
+                        }
+                        nr++;
+                        prev = (int)c;
+                    }
                 }
             }
-            if(ok==1)
-                Console.WriteLine($"poz {poz} ");
-            else
-                Console.WriteLine("-1");
+            if (cresc == nr && cresc != 0 && nr != 0)
+                grup++;
         }
+        Console.WriteLine($"Nr grupuri: {grup}");
 
-        private static void problema3()
-        {
-            int n= int.Parse(Console.ReadLine());
-            int s = 0, p = 1;
-            for (int i = 1; i <= n; i++)
-            {
-                s += i;
-                p *= i;
-            }
-            Console.WriteLine($"suma {s} prod {p}");
-        }
-
-        private static void problema2()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int nrn = 0, nr0 = 0, nrp = 0; ;
-            int[] v = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                v[i] = int.Parse(Console.ReadLine());
-                if (v[i] < 0)
-                    nrn++;
-                else if (v[i] > 0)
-                    nrp++;
-                else if (v[i] == 0)
-                    nr0++;
-            }
-            Console.WriteLine($"neg {nrn} poz {nrp} 0 {nr0}");
-        }
-
-        private static void problema1()
-        {
-            int n = int.Parse(Console.ReadLine());
-            int nr = 0;
-            int[] v = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                v[i] = int.Parse(Console.ReadLine());
-                if (v[i] % 2 == 0)
-                    nr++;
-            }
-            Console.WriteLine(nr);
-        }
     }
+
+    private static void problema11()
+    {
+        Console.WriteLine("n");
+        int n = int.Parse(Console.ReadLine());
+        int s = 0;
+        int invers;
+        for (int i = 0; i < n; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            invers = 0;
+            while (a != 0)
+            {
+                invers = invers * 10 + a % 10;
+                a = a / 10;
+            }
+            s += invers;
+        }
+        Console.WriteLine(s);
+    }
+
+    private static void problema10()
+    {
+        Console.WriteLine("n");
+        int n = int.Parse(Console.ReadLine());
+        int a = int.Parse(Console.ReadLine());
+        int ok = 1, maxim = 0; ;
+        for (int i = 1; i < n; i++)
+        {
+            int b = int.Parse(Console.ReadLine());
+            if (a == b)
+                ok++;
+            else
+                ok = 1;
+            if (ok >= maxim)
+                maxim = ok;
+        }
+        Console.WriteLine(maxim);
+    }
+
+    private static void problema9()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int okc = 0;
+        int okd = 0;
+        int a = int.Parse(Console.ReadLine());
+        for (int i = 1; i < n; i++)
+        {
+            int b = int.Parse(Console.ReadLine());
+            if (a <= b)
+                okc++;
+            else
+                okd++;
+            b = a;
+        }
+        if (okd == 0)
+            Console.WriteLine("Secventa monoton crescatoare");
+        else if (okc == 0)
+            Console.WriteLine("Secventa monoton descrescatoare");
+        else
+            Console.WriteLine("Secventa nu este monotona");
+    }
+
+    private static void problema8()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for (int i = 2; i < n; i++)
+        {
+            c = b + a;
+            a = b;
+            b = c;
+        }
+        Console.WriteLine($"al {n} lea nr este {c}");
+    }
+
+    private static void problema7()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] v = new int[n];
+        int b = int.Parse(Console.ReadLine());
+        int minim, maxim;
+        minim = maxim = b;
+        for (int i = 1; i < n; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a < minim)
+                minim = a;
+            else if (a > maxim)
+                maxim = a;
+        }
+        Console.WriteLine("minim " + minim);
+        Console.WriteLine("maxim " + maxim);
+    }
+
+    private static void problema6()
+    {
+        Console.WriteLine("n");
+        int n = int.Parse(Console.ReadLine());
+        int a = int.Parse(Console.ReadLine());
+        int nr = 0;
+        for (int i = 1; i < n; i++)
+        {
+            int b = int.Parse(Console.ReadLine());
+            if (b >= a)
+                nr = 0;
+            else
+                nr++;
+            a = b;
+        }
+        if (nr == 0)
+            Console.WriteLine("da");
+        else
+            Console.WriteLine("nu");
+    }
+
+    private static void problema5()
+    {
+        Console.WriteLine("n");
+        int n = int.Parse(Console.ReadLine());
+        int nr = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a == i)
+                nr++;
+        }
+        Console.WriteLine(nr);
+    }
+
+    private static void problema4()
+    {
+        Console.WriteLine("n");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("a");
+        int a = int.Parse(Console.ReadLine());
+        int ok = 0, poz = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int b = int.Parse(Console.ReadLine());
+            if (b == a)
+            {
+                ok = 1;
+                poz = i;
+            }
+        }
+        if (ok == 1)
+            Console.WriteLine($"poz {poz} ");
+        else
+            Console.WriteLine("-1");
+    }
+
+    private static void problema3()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int s = 0, p = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            s += i;
+            p *= i;
+        }
+        Console.WriteLine($"suma {s} prod {p}");
+    }
+
+    private static void problema2()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int nrn = 0, nr0 = 0, nrp = 0; ;
+        for (int i = 0; i < n; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a < 0)
+                nrn++;
+            else if (a > 0)
+                nrp++;
+            else if (a == 0)
+                nr0++;
+        }
+        Console.WriteLine($"neg {nrn} poz {nrp} 0 {nr0}");
+    }
+
+    private static void problema1()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int nr = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+                nr++;
+        }
+        Console.WriteLine(nr);
+    }
+}
 }
