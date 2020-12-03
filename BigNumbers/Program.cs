3ef2ee1df788a2894f5a04c273dc17ced52dff73
George Breje <george.breje2@gmail.com>
2020-12-03 17:17:16 +0200

    solving

---
+++ BigNumbers/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace BigNumbers
+{
+    class Program
+    {
+        public struct BigInteger : 
+
+        static void ReadOperation()
+        {
+            ConsoleKeyInfo input = Console.ReadKey();
+            switch (input.KeyChar)
+            {
+                case '1':
+                    Addition();
+                    break;
+                case '2':
+                    Substraction();
+                    break;
+                case '3':
+                    Multiplication();
+                    break;
+                case '4':
+                    Division();
+                    break;
+                case '5':
+                    Exponentiation();
+                    break;
+                case '6':
+                    SquareRoot();
+                    break;
+                default:
+                    Console.WriteLine("Choose a number from 1 to 6");
+                    break;
+
+
+            }
+        }
+
+        private static void Addition()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select operation by pressing its designated key");
+            Console.WriteLine("1 -> addition");
+            Console.WriteLine("2 -> substraction");
+            Console.WriteLine("3 -> multiplication");
+            Console.WriteLine("4 -> division");
+            Console.WriteLine("5 -> exponentiation");
+            Console.WriteLine("6 -> square root");
+            ReadOperation();
+            Console.WriteLine("Enter numbers on the same line with space separator");
+            ReadNumbers();
+        }
+
+        private static void ReadNumbers()
+        {
+            BigInteger number = 
+            string line = Console.ReadLine();
+            string[] split = line.Split(' ');
+            for(int i=0;i<split.Length;i++)
+            {
+
+            }
+        }
+    }
+}
--- pool3/Program.cs
+++ pool3/Program.cs
@@ -30,11 +30,337 @@ namespace pool3
             //problema17();
             //problema18();
             //problema19();
+            //problema20();
+            //problema21();
+            //problema22();
+            //problema23();
+            //problema24();
+            problema25();
+        }
+
+        private static void problema25()
+        {
+            Console.WriteLine("n,vector1");
+            int n = int.Parse(Console.ReadLine());
+            int[] v1 = new int[n];
+            for (int i = 0; i < n; i++)
+                v1[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("m,vector2");
+            int m = int.Parse(Console.ReadLine());
+            int[] v2 = new int[m];
+            for (int i = 0; i < m; i++)
+                v2[i] = int.Parse(Console.ReadLine());
+            int[] v3 = new int[n + m];
+            int l = 0, j = 0,k=0;
+            while(l<n && j<m)
+            {
+                if(v1[l]<v2[j])
+                {
+                    v3[k] = v1[l];
+                    k++;
+                    l++;
+                }
+                else
+                {
+                    v3[k] = v2[j];
+                    k++;
+                    j++;
+                }
+            }
+            if(l<n)
+            {
+                for(int p=l;p<n;p++)
+                {
+                    v3[k] = v1[p];
+                    k++;
+                }
+            }
+            if (j < m)
+            {
+                for (int p = j; p < m; p++)
+                {
+                    v3[k] = v2[p];
+                    k++;
+                }
+            }
+            Console.WriteLine(k);
+            Afisare(v3, k);
+        }
+
+        private static void problema24()
+        {
+            
+        }
+
+        private static void problema23()
+        {
+
+        }
+
+        private static void problema22()
+        {
+            int[] frecv = new int[100000];
+            int[] frecv1 = new int[100000];
+            int[] frecv2 = new int[100000];
+            for (int i = 0; i < 100000; i++)
+            {
+                frecv[i] = 0;
+                frecv1[i] = 0;
+                frecv2[i] = 0;
+            }
+            Console.WriteLine("n,vector1");
+            int n = int.Parse(Console.ReadLine());
+            int[] v1 = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                v1[i] = int.Parse(Console.ReadLine());
+                frecv[v1[i]]++;
+                frecv1[v1[i]]++;
+            }
+            Console.WriteLine("m,vector2");
+            int m = int.Parse(Console.ReadLine());
+            int[] v2 = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                v2[i] = int.Parse(Console.ReadLine());
+                frecv[v2[i]]++;
+                frecv2[v2[i]]++;
+            }
+            int p = n + m;
+            int[] v3 = new int[p];
+            int j = 0;
+            //reuniune
+
+            for (int i = 0; i < 100000; i++)
+            {
+                while (frecv[i] > 1)
+                {
+                    if (frecv[i] == 1)
+                        p--;
+                    else
+                        p--;
+                    frecv[i]--;
+
+                }
+                if (frecv[i] == 1 || frecv[i] == 1)
+                {
+                    v3[j] = i;
+                    j++;
+                }
+            }
+            Console.WriteLine($"p = {p}");
+            Console.WriteLine("Reuniune:");
+            Afisare(v3, p);
+            //intersectie
+            p = n + m;
+            int j1 = 0, ps = 0;
+            int[] v4 = new int[p];
+
+            for (int i = 0; i < 100000; i++)
+            {
+                if (frecv1[i] != 0 && frecv2[i] != 0)
+                {
+                    v4[j1] = i;
+                    j1++;
+                    ps++;
+                }
+            }
+            Console.WriteLine("\nIntersectia:");
+            Afisare(v4, ps);
+            //diferenta v1/v2
+            int[] v5 = new int[p];
+            int j2 = 0,ps1=0;
+            for (int i = 0; i < 10000; i++)
+            {
+                if (frecv1[i] != 0 && frecv2[i] == 0)
+                { 
+                    v5[j2] = i;
+                    j2++;
+                    ps1++;
+                }
+            }
+            Console.WriteLine("\nDiferenta v1-v2");
+            Afisare(v5, ps1);
+            int[] v6 = new int[p];
+            int j3 = 0, ps2 = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                if (frecv1[i] == 0 && frecv2[i] != 0)
+                {
+                    v6[j3] = i;
+                    j3++;
+                    ps2++;
+                }
+            }
+            Console.WriteLine("\nDiferenta v2-v1");
+            Afisare(v6, ps2);
+        }
+
+
+        private static void problema21()
+        {
+            Console.WriteLine("n,vector1");
+            int n = int.Parse(Console.ReadLine());
+            int[] v1 = new int[n];
+            Citire(v1, n);
+            Console.WriteLine("m,vector2");
+            int m = int.Parse(Console.ReadLine());
+            int[] v2 = new int[m];
+            Citire(v2, m);
+            int nr = 0;
+            if (n > m)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    if (v1[i] > v2[i])
+                    {
+                        Console.WriteLine("V2 este primul");
+                        break;
+                    }
+                    else if (v2[i] > v1[i])
+                    {
+                        Console.WriteLine("V1 este primul");
+                        break;
+                    }
+                    else if (v1[i] == v2[i])
+                    {
+                        nr++;
+                    }
+                }
+                if (nr == m)
+                {
+                    Console.WriteLine("V2 este primul");
+                }
+            }
+            else
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    if (v2[i] > v1[i])
+                    {
+                        Console.WriteLine("V1 este primul");
+                        break;
+                    }
+                    else if (v1[i] > v2[i])
+                    {
+                        Console.WriteLine("V2 este primul");
+                        break;
+                    }
+                    else if (v1[i] == v2[i])
+                    {
+                        nr++;
+                    }
+                }
+                if (nr == n)
+                {
+                    if (n == m)
+                        Console.WriteLine("sunt egale");
+                    else
+                        Console.WriteLine("V1 este primul");
+                }
+            }
+        }
+        public static void Rotire(int[] A, int n)
+        {
+            int aux;
+            for (int i = 0; i < n - 1; i++)
+            {
+                aux = A[i];
+                A[i] = A[i + 1];
+                A[i + 1] = aux;
+            }
+        }
+        public static void Citire(int[] A, int n)
+        {
+
+            for (int i = 0; i < n; i++)
+                A[i] = int.Parse(Console.ReadLine());
+        }
+        public static void Afisare(int[] A, int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.Write($"{A[i]} ");
+        }
+        private static int CautareVector(int[] A, int[] B, int max, int min)
+        {
+            int nr = 0;
+            for (int i = 0; i < max - min + 1; i++)
+            {
+                int j;
+                for (j = 0; j < min && A[j + i] == B[j]; j++) { }
+                if (j == min)
+                    nr++;
+            }
+            return nr;
+        }
+        private static void problema20()
+        {
+            Console.WriteLine("n,vector1");
+            int n = int.Parse(Console.ReadLine());
+            int[] v1 = new int[n];
+            Citire(v1, n);
+            Console.WriteLine("m,vector2");
+            int m = int.Parse(Console.ReadLine());
+            int[] v2 = new int[m];
+            Citire(v2, m);
+            int nr = 0;
+            if (n > m)
+            {
+                for (int i1 = 0; i1 < n - 1; i1++)
+                {
+                    if (CautareVector(v1, v2, n, m) != 0)
+                    {
+                        nr++;
+                    }
+
+                    Rotire(v1, n);
+                    Console.WriteLine();
+                    Console.WriteLine($"Vectorul dupa rotirea numarul {i1} este");
+                    Afisare(v1, n);
+                }
+            }
+            else
+            {
+                for (int i2 = 0; i2 < m - 1; i2++)
+                {
+                    if (CautareVector(v2, v1, m, n) != 0)
+                    {
+                        nr++;
+                    }
+
+                    Rotire(v2, m);
+                    Console.WriteLine();
+                    Console.WriteLine($"Vectorul dupa rotirea numarul {i2} este");
+                    Afisare(v2, m);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Numarul de suprapuneri margea cu margea este {nr}");
         }
 
         private static void problema19()
         {
-
+            Console.WriteLine("lungime vector in care se cauta");
+            int n = int.Parse(Console.ReadLine());
+            int[] A = new int[n];
+            Console.WriteLine("vector in care se cauta");
+            for (int i = 0; i < n; i++)
+                A[i] = int.Parse(Console.ReadLine());
+            Console.WriteLine("lungime vector care se cauta");
+            int m = int.Parse(Console.ReadLine());
+            int[] B = new int[m];
+            Console.WriteLine("vector care se cauta");
+            for (int i = 0; i < m; i++)
+                B[i] = int.Parse(Console.ReadLine());
+            int nr = 0;
+            for (int i = 0; i < n - m + 1; i++)
+            {
+                int j;
+                for (j = 0; j < m && A[j + i] == B[j]; j++) { }
+                if (j == m)
+                    nr++;
+            }
+            Console.WriteLine(nr);
         }
 
         private static void problema18()
@@ -45,13 +371,13 @@ namespace pool3
             Console.WriteLine("Introduceti punctul x");
             int x = int.Parse(Console.ReadLine());
             Console.WriteLine("Coeficientii (n+1 coeficienti)");
-            int[] coef = new int[n+1];
+            int[] coef = new int[n + 1];
             int suma = 0;
-            for(int i=0;i<n+1;i++)
-                coef[i]=int.Parse(Console.ReadLine());
+            for (int i = 0; i < n + 1; i++)
+                coef[i] = int.Parse(Console.ReadLine());
             for (int i = 0; i < n + 1; i++)
             {
-                suma += coef[i] * (int)(Math.Pow(x,nCopy));
+                suma += coef[i] * (int)(Math.Pow(x, nCopy));
                 if (i < n)
                 {
                     Console.Write($"{coef[i]}*{x}^{nCopy} + ");
@@ -85,8 +411,8 @@ namespace pool3
             {
                 if (v[j] < 10)
                     Console.Write(v[j]);
-                else if(v[j]>=10)
-                    Console.Write((char)(v[j] + (int)'A'-10));
+                else if (v[j] >= 10)
+                    Console.Write((char)(v[j] + (int)'A' - 10));
             }
         }
 
@@ -126,7 +452,7 @@ namespace pool3
                 {
                     if (A[i] == A[j])
                     {
-                        for(int k=j;k<n-1;k++)
+                        for (int k = j; k < n - 1; k++)
                         {
                             A[k] = A[k + 1];
                         }
@@ -135,7 +461,7 @@ namespace pool3
                     }
                 }
             }
-            for(int i=0;i<n;i++)
+            for (int i = 0; i < n; i++)
                 Console.Write($"{A[i]} ");
             /* varianta in care niciun element care se repeta nu mai apare
             int n = int.Parse(Console.ReadLine());
@@ -426,7 +752,6 @@ namespace pool3
             int[] A = new int[n];
             int max;
             int min;
-            int count = 0;
             for (int i = 0; i < n; i++)
                 A[i] = int.Parse(Console.ReadLine());
             min = max = A[0];
