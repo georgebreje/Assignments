a1dff782ac2fd0c0029030182718d20175f07de3
George Breje <george.breje2@gmail.com>
2020-12-26 18:23:29 +0200

    regex

---
+++ ARC/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ARC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            StreamReader file = new StreamReader(@"data.in");
+            while ((line = file.ReadLine()) != null)
+            {
+                
+            }
+        }
+    }
+}
