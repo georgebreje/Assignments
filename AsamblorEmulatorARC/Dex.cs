4a4ea18e6ec1ee6da44ee670d3f9843bda06aaf1
George Breje <george.breje2@gmail.com>
2020-12-26 17:40:27 +0200

    add blueprint

---
+++ AsamblorEmulatorARC/Dex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Dexy
+{
+	public class Dex
+	{
+		public static void nume(string line)
+		{
+			string programDelimitationPattern = @"\b[.]\w+";
+			Regex line = new Regex(programDelimitationPattern);
+			matches = programDelimitation.Matches(programDelimitationPattern);
+			for (int i = 0; i < matches.Count; i++)
+				Console.WriteLine($"{matches[i].Value}");
+		}
+	}
+}
---
+++ AsamblorEmulatorARC/UserSpace.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class UserSpace
+{
+	public UserSpace()
+	{
+		
+	}
+}
